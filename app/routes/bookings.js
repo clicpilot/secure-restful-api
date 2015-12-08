07eedc71e2384666d8061d50b7d2bfa0ab1e6928
hhtopcu <hasanht1@gmail.com>
2015-12-08 11:18:27 +0200

    All routes are added

--- app/routes/bookings.js
+++ app/routes/bookings.js
@@ -3,16 +3,9 @@ var router = express.Router();
 
 var Booking     = require('../models/booking');
 
-/* GET users listing. 
-router.get('/bookings', function(req, res, next) {
-  res.json({ message: 'Booking created!' });
-});
-*/
 
 router.route('/bookings')
-
-
-	// get all the bears (accessed at GET http://localhost:8080/api/bears)
+    // get all the bears (accessed at GET http://localhost:8080/api/bears)
     .get(function(req, res) {
         Booking.find(function(err, bookings) {
             if (err)
@@ -21,8 +14,7 @@ router.route('/bookings')
             res.json(bookings);
         });
     })
-
-    // create a bear (accessed at POST http://localhost:8080/api/bears)
+    // create a booking (accessed at POST http://localhost:8080/api/bears)
     .post(function(req, res) {
         
         var booking = new Booking();      // create a new instance of the Bear model
@@ -37,5 +29,50 @@ router.route('/bookings')
         });
         
     });
+
+
+router.route('/bookings/:booking_id')    
+    // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
+    .get(function(req, res) {
+        Booking.findById(req.params.booking_id, function(err, booking) {
+            if (err)
+                res.send(err);
+
+            res.json(booking);
+        });
+    })
+    // update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
+    .put(function(req, res) {
+
+        // use our booking model to find the bear we want
+        Booking.findById(req.params.booking_id, function(err, booking) {
+
+            if (err)
+                res.send(err);
+
+            booking.name = req.body.name;  // update the bears info
+
+            // save the bear
+            booking.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Booking updated!' });
+            });
+
+        });
+    })
+    // delete the bear with this id (accessed at DELETE http://localhost:8080/api/bears/:bear_id)
+    .delete(function(req, res) {
+        Booking.remove({
+            _id: req.params.booking_id
+        }, function(err, booking) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Successfully deleted' });
+        });
+    });
+
 
 module.exports = router;
