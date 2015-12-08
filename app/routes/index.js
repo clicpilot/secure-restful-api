9bb2cce9debf198fd4b7ff6b9c812313c698ba3b
hhtopcu <hasanht1@gmail.com>
2015-12-08 15:05:55 +0200

    Access Token generation is added

---
+++ app/routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+ 
+var auth = require('./auth.js');
+var bookings = require('./bookings.js');
+//var user = require('./users.js');
+ 
+/*
+ * Routes that can be accessed by any one
+ */
+router.post('/login', auth.login);
+ 
+/*
+ * Routes that can be accessed only by autheticated users
+ */
+router.get('/v1/bookings', bookings.getAll);
+router.get('/v1/booking/:id', bookings.getOne);
+router.post('/v1/booking/', bookings.create);
+router.put('/v1/booking/:id', bookings.update);
+router.delete('/v1/booking/:id', bookings.delete);
+ 
+/*
+ * Routes that can be accessed only by authenticated & authorized users
+ */
+
+/*
+router.get('/api/v1/admin/users', user.getAll);
+router.get('/api/v1/admin/user/:id', user.getOne);
+router.post('/api/v1/admin/user/', user.create);
+router.put('/api/v1/admin/user/:id', user.update);
+router.delete('/api/v1/admin/user/:id', user.delete);
+*/
+ 
+module.exports = router;
--- server.js
+++ server.js
@@ -2,17 +2,55 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var logger = require('morgan');
 var mongoose   = require('mongoose');
 
 // Module for Booking Model
-var bookingRoute = require('./app/routes/bookings')
+//var bookingRoute = require('./app/routes/bookings')
 
 mongoose.connect('mongodb://admin:1qaz.2wsx.@ds039421.mongolab.com:39421/kinetica');
+
+// Logger for development
+app.use(logger('dev'));
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Enable Cross Origin Resource Sharing
+app.all('/*', function(req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  // Set custom headers for CORS
+  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+
+  if (req.method == 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
+
+// Auth Middleware - This will check if the token is valid
+// Only the requests that start with /api/v1/* will be checked for the token.
+// Any URL's that do not follow the below pattern should be avoided unless you 
+// are sure that authentication is not needed
+app.all('/v1/*', [require('./app/middlewares/validateRequest')]);
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+app.use('/', require('./app/routes'));
+ 
+// If no route is matched by now, it must be a 404
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
 
 var port = process.env.PORT || 8080;        // set our port
 
@@ -33,11 +71,7 @@ router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
-// more routes for our API will happen here
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', bookingRoute);
 
 // START THE SERVER
 // =============================================================================
